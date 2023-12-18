6950d9d710a4923bbf3367f34120ee40050dd0d3
crackgithubcom <153576636+crackgithubcom@users.noreply.github.com>
2023-12-18 22:24:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,58 @@
         ["May I try this on, please?", "我可以试穿一下吗？"]
     ];
 
+    var mealData = [
+        ["Do I need a reservation?", "需要预定吗？"],
+        ["I'd like to order [菜名], please.", "我想要点这个。"],
+        ["I'm ready to pay now. Could you bring me the bill?", "我准备付钱了，可以给我帐单吗？"]
+    ];
+
+    var askdirData = [
+        ["Is it within walking distance?", "我们可以走路过去吗？"],
+        ["Excuse me, can you tell me how to get to [地点]?", "请问这里要怎么去呢"]
+    ];
+
+    var xData = [
+        ["", ""],
+        ["", ""]
+    ];
+
     // Call the function with different data for each section
     generateTable("在机场", airportData);
     generateTable("打车与交通", taxiData);
     generateTable("购物", shoppingData);
+    generateTable("吃饭", mealData);
+    generateTable("问路", askdirData);
+
+/*
+
+        
+        <h2>问路：</h2>
+        <ul>
+            <li>""</li>
+            <li>"Is it within walking distance?"</li>
+            <li>"Which direction is [地标]?"</li>
+        </ul>
+        
+        <h2>使用Uber或打车应用：</h2>
+        <ul>
+            <li>"I would like to book a ride to [目的地], please."</li>
+            <li>"How long will it take to get there?"</li>
+            <li>"Could you please drop me off at [位置]?"</li>
+        </ul>
+        
+        <h2>在旅馆/酒店：</h2>
+        <ul>
+            <li>办理入住手续： "I have a reservation under [你的名字]."</li>
+            <li>房间设施： "Is breakfast included?"</li>
+            <li>询问服务： "Could you please send someone to clean the room?"</li>
+            <li>结账离店： "What time is check-out?"</li>
+        </ul>
+        
+        <h2>紧急情况：</h2>
+        <ul>
+            <li>"I need help. Can you please call the police/ambulance?"</li>
+            <li>"Is there a hospital near here?"</li>
+            <li>"I've lost my [物品]. Can you help me find it?"</li>
+        </ul>
+*/
