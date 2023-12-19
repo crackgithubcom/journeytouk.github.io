d6221cf0186fc27f73c2c78f91d28f2e5f126284
crackgithubcom <153576636+crackgithubcom@users.noreply.github.com>
2023-12-19 15:08:56 +0800

    Update utterance.js

--- utterance.js
+++ utterance.js
@@ -1,1 +1,15 @@
+// Function to read general text
+function readText(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+}
 
+// Get all elements with the "readable" class
+const readableElements = document.querySelectorAll('.readable');
+
+// Add click event listener to each readable element
+readableElements.forEach(element => {
+    element.addEventListener('click', () => {
+        readText(element.innerText);
+    });
+});
