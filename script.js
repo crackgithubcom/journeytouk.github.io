da8dc96e899cf28f155d72da9e1a31ae076fad63
crackgithubcom <153576636+crackgithubcom@users.noreply.github.com>
2023-12-19 15:08:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -70,24 +70,6 @@ generateTable("吃饭", mealData);
 generateTable("问路", askdirData);
 
 
-
-// Function to read general text
-function readText(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
-    speechSynthesis.speak(utterance);
-}
-
-// Get all elements with the "readable" class
-const readableElements = document.querySelectorAll('.readable');
-
-// Add click event listener to each readable element
-readableElements.forEach(element => {
-    element.addEventListener('click', () => {
-        readText(element.innerText);
-    });
-});
-
-
 /*
 
         
