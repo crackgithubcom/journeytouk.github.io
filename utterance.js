2720e53d9fb59bf8501e8ad7f640413241d90dbf
boris jobs <1322553126@qq.com>
2024-01-17 14:12:51 +0800

    Update utterance.js

--- utterance.js
+++ utterance.js
@@ -1,4 +1,4 @@
-/*
+
 // 获取所有的语音合成器声音  
 const voices = speechSynthesis.getVoices();  
   
@@ -26,10 +26,10 @@ readableElements.forEach(element => {
     });  
 });
 
-*/
 
 
 
+/*
 // Function to read general text
 function readText(text) {
     const utterance = new SpeechSynthesisUtterance(text);
@@ -45,5 +45,5 @@ readableElements.forEach(element => {
         readText(element.innerText);
     });
 });
-
+*/
 
