27f74e4894d52763079d8399b90ecb1730d1d60d
boris jobs <1322553126@qq.com>
2024-01-17 12:24:35 +0800

    Update utterance.js

--- utterance.js
+++ utterance.js
@@ -1,29 +1,32 @@
-function readText(text) {
-    const utterance = new SpeechSynthesisUtterance(text);
-
-    // 为了在 iPhone 上工作，可能需要设置语言为 'en-US' 或其他支持的语言
-    utterance.lang = 'en-AU';
-
-    // 在 iPhone 上，尝试选择支持的语音（例如 Siri 的语音）
-    const iosVoice = speechSynthesis.getVoices().find(voice => voice.lang === 'en-AU');
-    
-    if (iosVoice) {
-        utterance.voice = iosVoice;
-    }
-
-    // 在 IE 上，SpeechSynthesisUtterance 不支持 'voice' 属性
-    if (window.speechSynthesis.speak !== undefined) {
-        speechSynthesis.speak(utterance);
-    } else {
-        // 在 IE 上使用其他兼容的方法来朗读文本
-        // 例如可以使用 <speak> 标签
-        // 注意：这是一种不同的方法，可能需要根据具体情况进一步调整
-        var speakElement = document.createElement('speak');
-        speakElement.innerText = text;
-        document.body.appendChild(speakElement);
-        speakElement.click();  // 触发朗读
-        document.body.removeChild(speakElement);
-    }
+function readText(text) {  
+    const utterance = new SpeechSynthesisUtterance(text);  
+  
+    // 选择男性声音  
+    const maleVoices = speechSynthesis.getVoices().filter(voice => voice.gender === 'male');  
+    if (maleVoices.length > 0) {  
+        utterance.voice = maleVoices[0];  
+    }  
+  
+    // 在 iPhone 上，尝试选择支持的语音（例如 Siri 的语音）  
+    const iosVoice = speechSynthesis.getVoices().find(voice => voice.lang === 'en-GB');  
+      
+    if (iosVoice) {  
+        utterance.voice = iosVoice;  
+    }  
+  
+    // 在 IE 上，SpeechSynthesisUtterance 不支持 'voice' 属性  
+    if (window.speechSynthesis.speak !== undefined) {  
+        speechSynthesis.speak(utterance);  
+    } else {  
+        // 在 IE 上使用其他兼容的方法来朗读文本  
+        // 例如可以使用 <speak> 标签  
+        // 注意：这是一种不同的方法，可能需要根据具体情况进一步调整  
+        var speakElement = document.createElement('speak');  
+        speakElement.innerText = text;  
+        document.body.appendChild(speakElement);  
+        speakElement.click();  // 触发朗读  
+        document.body.removeChild(speakElement);  
+    }  
 }
 
 // 在获取语音列表后再执行读取文本的操作
